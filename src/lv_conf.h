e08a54bfb6c941334388e951ab6fbc9f0e05e8ae
Cdnspeed <65375468+Cdnspeed@users.noreply.github.com>
2025-11-11 12:44:54 -0500

    Enable Montserrat fonts 40, 46, and 48

--- src/lv_conf.h
+++ src/lv_conf.h
@@ -118,11 +118,11 @@
 #define LV_FONT_MONTSERRAT_34       0
 #define LV_FONT_MONTSERRAT_36       0
 #define LV_FONT_MONTSERRAT_38       0
-#define LV_FONT_MONTSERRAT_40       0
+#define LV_FONT_MONTSERRAT_40       1
 #define LV_FONT_MONTSERRAT_42       0
 #define LV_FONT_MONTSERRAT_44       0
-#define LV_FONT_MONTSERRAT_46       0
-#define LV_FONT_MONTSERRAT_48       0
+#define LV_FONT_MONTSERRAT_46       1
+#define LV_FONT_MONTSERRAT_48       1
 
 #define LV_USE_BIDI                 0
 #define LV_USE_ARABIC_PERSIAN_CHARS 0
